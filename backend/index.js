a41bf35a5fdd708871350fafeedd1a725fb510f5
czpeng822 <153256821+czpeng822@users.noreply.github.com>
2025-03-22 20:08:25 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use('/files', express.static('files'))
 const uri = process.env.MONGODB_URI
 
 const corsOptions = {
-    origin: 'https://comprehensiveonlineexamplatform.onrender.com',
+    origin: 'https://testopia.onrender.com/',
     credentials: true,
     maxAge: 200
 };
